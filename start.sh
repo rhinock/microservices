d31dde15d201c996649a37e3ab123760439c8004
Rinat Khabibulin <rhinock@yandex.ru>
2021-09-30 13:35:23 +0300

    fix .sh line

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-/usr/bin/mongod --fork --logpath
-/var/log/mongod.log --config /etc/mongodb.conf
+/usr/bin/mongod --fork --logpath /var/log/mongod.log --config /etc/mongodb.conf
 source /app/db_config
 cd /app && puma || exit
